7cf512c014e27c6daf2ec37055246a1e3c1022d2
Thirumurthi <thirumurthi.s@gmail.com>
2021-11-22 23:19:42 -0800

    included integration test java 11 client based validation

--- src/main/java/com/stock/finance/config/SpringSecurityConfig.java
+++ src/main/java/com/stock/finance/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.stock.finance.filter.JwtRequestFilter;
 
+@EnableWebSecurity
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
